0a886d22be1f4eb931056949dfeb0ccbdd69e624
Arief Rama Syarif <arief.rama.s@gmail.com>
2020-12-22 10:39:03 +0700

    Koneksi ke Cloud SQL - GCP

--- lib/api.dart
+++ lib/api.dart
@@ -1,4 +1,4 @@
 class Api {
-  static String url = "http://192.168.43.212/flutter_login/check_db.php";
-  static String regis = "http://192.168.43.212/flutter_login/register_db.php";
+  static String url = "http://34.101.156.50/sewaaja/check_db.php";
+  static String regis = "http://34.101.156.50/sewaaja/register_db.php";
 }
